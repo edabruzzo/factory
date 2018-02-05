9ee1453aee9a4b7929ce6b447528e6dcab865cb8
edabruzzo <emmanuel.oliveira3@gmail.com>
2018-02-05 10:03:17 -0200

    Utilização da anotação Query_ para criação de queries nativas - uso limitado, objetivo de mero aprendizado

---
+++ src/main/java/factory/QueryFactory.java
@@ -0,0 +1,40 @@
+package factory;
+
+import java.lang.reflect.ParameterizedType;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+import javax.persistence.Query;
+
+import jpaAnnotations.Query_;
+import jsfAnnotation.ScopeMap;
+import jsfAnnotation.ScopeMap.Scope;
+
+
+
+public class QueryFactory {
+
+
+	@Inject
+	JPAFactory jpa;
+	
+	@SuppressWarnings("rawtypes")
+	@Produces
+	@Query_("")
+	public Query factory(InjectionPoint pontoInjecao) {
+		
+	String sqlString = pontoInjecao.getAnnotated().getAnnotation(Query_.class).value();
+
+	ParameterizedType type = (ParameterizedType) pontoInjecao.getType();
+	Class classe = type.getClass();
+
+	return jpa.getEntityManager().createNativeQuery(sqlString, classe);
+		
+	}
+	
+	
+}
+
+	
+	
